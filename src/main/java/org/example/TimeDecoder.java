dc391dea79ef8cf1dc7b7149c094fe5ae4c4eadd
ming.wang4 <ming.wang4@envision-digital.com>
2022-11-01 19:26:09 +0800

    pojo

---
+++ src/main/java/org/example/TimeDecoder.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ReplayingDecoder;
+
+import java.util.List;
+
+public class TimeDecoder extends ReplayingDecoder<Void> {
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
+        if (in.readableBytes() < 4) {
+            return;
+        }
+
+        out.add(new UnixTime(in.readUnsignedInt()));
+    }
+}
---
+++ src/main/java/org/example/TimeEncoder2.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class TimeEncoder2 extends MessageToByteEncoder<UnixTime> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, UnixTime msg, ByteBuf out) {
+        out.writeInt((int)msg.value());
+    }
+}
